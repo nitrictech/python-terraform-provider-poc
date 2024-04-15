ad1c93f7af53154ac183dd89c80b876e2c989abe
Jye Cusch <jye.cusch@nitric.io>
2024-04-16 08:31:42 +1000

    fix asyncio deprecation warning

--- provider/main.py
+++ provider/main.py
@@ -10,5 +10,6 @@ async def main():
     await server.wait_closed()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main())

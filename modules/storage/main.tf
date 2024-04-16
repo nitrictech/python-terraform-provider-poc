69eeb0a12de3d0af939076a451accff161c6b735
Tim Holm <tim.holm@nitric.io>
2024-04-17 09:01:28 +1000

    add random id to bucket name

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,6 +1,17 @@
+
+# Generate a random id for the bucket
+resource "random_id" "bucket_id" {
+  byte_length = 8
+
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    bucket_name = var.bucket_name
+  }
+}
+
 # A Google cloud storage bucket
 resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
+  name          = "${var.bucket_name}-${random_id.bucket_id.hex}"
   location      = var.bucket_location
   project       = var.project_id
   storage_class = var.storage_class
